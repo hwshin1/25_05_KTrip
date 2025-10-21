70d724140ef56c0fde66dacbc9e3fbf990f44124
hwshin1 <hwshin5044@gmail.com>
2025-10-21 16:27:32 +0900

    review 부분 jsp => Thymeleaf로 변환중2

--- src/main/java/org/myproject/demo/repository/ReviewRepository.java
+++ src/main/java/org/myproject/demo/repository/ReviewRepository.java
@@ -3,7 +3,14 @@ package org.myproject.demo.repository;
 import org.myproject.demo.vo.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    int getReviewCount(int boardId, String searchKeywordTypeCode, String searchKeyword);
+
+    List<Review> getForPrintReviews(int boardId, int limitFrom, int limitTake, String searchKeywordTypeCode, String searchKeyword);
+
+    Review getForPrintReview(Long id);
     /* mybatis 방식
     int doWrite(int userId, String title, String body, String boardId);
 
