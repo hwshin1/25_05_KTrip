225d8d7740b6a23f24ce5bf82f6234459b013a95
hwshin1 <hwshin5044@gmail.com>
2025-11-10 23:23:39 +0900

    리뷰 상세보기 오류 생성 및 ReviewService userCanModify 부분 review.getId()로 수정

--- src/main/java/org/myproject/demo/service/ReviewService.java
+++ src/main/java/org/myproject/demo/service/ReviewService.java
@@ -76,10 +76,10 @@ public class ReviewService {
 
     public ResultData userCanModify(Long loginedMemberId, Review review) {
         if (review.getId() != loginedMemberId) {
-            return ResultData.from("F-A", Ut.f("%d번 리뷰에 대한 수정 권한 없음", review));
+            return ResultData.from("F-A", Ut.f("%d번 리뷰에 대한 수정 권한 없음", review.getId()));
         }
 
-        return ResultData.from("S-1", Ut.f("%d번 리뷰가 수정되었습니다.", review));
+        return ResultData.from("S-1", Ut.f("%d번 리뷰가 수정되었습니다.", review.getId()));
     }
 
     public ResultData userCanDelete(Long loginedMemberId, Review review) {
